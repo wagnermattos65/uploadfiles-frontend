9d1d4d11a57ef50082d2e0947172bf71b935fe19
Wagner Mattos <github@wagnermattos.com>
2019-06-21 10:48:39 -0300

    feat: change upload message

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -7,7 +7,7 @@ import { DropContainer, UploadMessage } from './styles';
 export default class Upload extends Component {
   renderDragMessage = ( isDragActive, isDragReject ) => {
     if (!isDragActive) {
-      return <UploadMessage>Arraste as imagens aqui...</UploadMessage>
+      return <UploadMessage>Arraste as imagens aqui ou clique para escolher os arquivos...</UploadMessage>
     }
 
     if (isDragReject) {
